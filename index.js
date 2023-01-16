3f30da9a67ef4ba2f3012d941eb33485f53d53c7
LCH6890 <lorenachamorroalameda@gmail.com>
2023-01-16 17:34:51 +0100

    Se añade ejercicio 14 JavaScript

---
+++ index.js
@@ -0,0 +1,8 @@
+const boton = document.querySelector("button")
+
+boton.addEventListener("click", () => alert("click en el botón"))
+
+
+$(".btn").click(function() {
+    console.log("Hola, estoy utilizando jQuery.")
+    })
